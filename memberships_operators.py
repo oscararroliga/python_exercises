1738119699cafd070ab4909cfcf99f6753f58bc2
oscararroliga <arroliga.oscar@gmail.com>
2023-06-07 11:19:16 +0100

    colors

---
+++ memberships_operators.py
@@ -0,0 +1,18 @@
+colors= ("red")
+
+user_color = input("What color do you need?")
+
+if user_color == colors:
+    print("Perfect, you can use your prefered color")
+
+else: print("Sorry, at this momment we dont have your prefered color")
+
+
+""" if "pink" in colors:
+    print("This color is present in the list")
+
+if purple not in colors:
+    print("This color is not present in the list")
+ """
+
+    
