b41c399ae65851a5b963e4c68f1a9ab8f99c7aca
oscararroliga <arroliga.oscar@gmail.com>
2023-06-07 16:31:05 +0100

    comments

--- for loop/characters_count.py
+++ for loop/characters_count.py
@@ -1,10 +1,14 @@
+##Catch the text to count
 string = input("Enter the words to count: ")
 
 count = 0
 
+##Iterate with the for loop
 for i in string:
     count = count + 1
-print("La cantidad de carácteres en tu contenido es " + str(count))
+print("The numbers of characters in your text is: " + str(count))
+
+##Warning mode, in case we need to notice an exceed of characters. 
 
 if count > 200:
     print("Content too big")
