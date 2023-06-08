e2199af9e0dd094c04ee9f6240855e85343243d8
oscararroliga <arroliga.oscar@gmail.com>
2023-06-08 16:56:03 +0100

    break

---
+++ conditional statements and loops/break.py
@@ -0,0 +1,6 @@
+for char in 'icecream':
+    if char == 't':
+        print('The letter %s was found in the string'  % char)
+        break
+else:
+    print('The letter %s does not exist in the string' % char)              
