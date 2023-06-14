e488dec1f6de18eb7728887f1ca02be4b9358e47
oscararroliga <arroliga.oscar@gmail.com>
2023-06-14 12:00:33 +0100

    Villain game

---
+++ conditional statements and loops/BreakStatementInForLoop.py
@@ -0,0 +1,9 @@
+places=["New Zeland", "Norway", "Botswana", "Zimbabwe", "Uzbekistan", "Nicaragua"]
+villain_at="Nicaragua"
+
+for place in places: 
+    if place== villain_at:
+        print("The Villain has been captured!")
+        break
+else:
+    print("The Villain got away :(")    
